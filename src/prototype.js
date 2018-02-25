37dd33673cb1627a4f04afdd692fec4f897300e2
William <wtpwinberg@gmail.com>
2018-02-24 23:09:34 -0800

    Complete prototype

--- src/prototype.js
+++ src/prototype.js
@@ -1,53 +1,72 @@
-/*
-  Object oriented design is commonly used in video games.  For this part of the assignment
-  you will be implementing several classes with their correct inheritance heirarchy.
 
-  In this file you will be creating three classes:
-  GameObject
-    createdAt
-    dimensions
-    destroy() // prototype method -> returns the string 'Game object was removed from the game.'
+// Object oriented design is commonly used in video games.  For this part of the assignment
+// you will be implementing several classes with their correct inheritance heirarchy.
 
-  NPC
-    hp
-    name
-    takeDamage() // prototype method -> returns the string '<object name> took damage.'
-    // should inherit destroy() from GameObject's prototype
+// In this file you will be creating three classes:
+// GameObject
+//   createdAt
+//   dimensions
+//   destroy() // prototype method -> returns the string 'Game object was removed from the game.'
+function GameObject(attrs) {
+  this.createdAt = attrs.createdAt;
+  this.dimensions = attrs.dimensions;
+  this.destroy = () => {
+    return 'Game object was removed from the game';
+  };
+}
+// NPC
+//   hp
+//   name
+//   takeDamage() // prototype method -> returns the string '<object name> took damage.'
+//   // should inherit destroy() from GameObject's prototype
+function NPC(npcAttrs) {
+  GameObject.call(this, npcAttrs);
+  this.hp = npcAttrs.hp;
+  this.name = npcAttrs.name;
+  this.takeDamage = () => {
+    return `${this.name} took damage.`;
+  };
+}
+// Humanoid
+//   faction
+//   weapons
+//   language
+//   greet() // prototype method -> returns the string '<object name> offers a greeting in <object language>.'
+//   // should inherit destroy() from GameObject through NPC
+//   // should inherit takeDamage() from NPChume
+function Humanoid(humeAttrs) {
+  NPC.call(this, humeAttrs);
+  this.faction = humeAttrs.faction;
+  this.weapons = humeAttrs.weapons;
+  this.language = humeAttrs.language;
+  this.greet = () => `${this.name} offers a greeting in ${this.language}.`;
+}
+// Inheritance chain: Humanoid -> NPC -> GameObject
+// Instances of Humanoid should have all of the same properties as NPC and GameObject.
+// Instances of NPC should have all of the same properties as GameObject.
 
-  Humanoid
-    faction
-    weapons
-    language
-    greet() // prototype method -> returns the string '<object name> offers a greeting in <object language>.'
-    // should inherit destroy() from GameObject through NPC
-    // should inherit takeDamage() from NPC
+// Example:
 
-  Inheritance chain: Humanoid -> NPC -> GameObject
-  Instances of Humanoid should have all of the same properties as NPC and GameObject.
-  Instances of NPC should have all of the same properties as GameObject.
+const hamsterHuey = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 2,
+    width: 1,
+    height: 1,
+  },
+  hp: 5,
+  name: 'Hamster Huey',
+  faction: 'Gooey Kablooie',
+  weapons: [
+    'bubblegum',
+  ],
+  language: 'Hamsterish',
+});
 
-  Example:
+console.log(hamsterHuey.greet()); // returns 'Hamster Huey offers a greeting in Hamsterish'
+console.log(hamsterHuey.takeDamage()); // returns 'Hamster Huey took damage.'
+console.log(hamsterHuey.destroy()); // returns 'Game object was removed from the game.'
 
-  const hamsterHuey = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 2,
-      width: 1,
-      height: 1,
-    },
-    hp: 5,
-    name: 'Hamster Huey',
-    faction: 'Gooey Kablooie',
-    weapons: [
-      'bubblegum',
-    ],
-    language: 'Hamsterish',
-  });
-
-  hamsterHuey.greet(); // returns 'Hamster Huey offers a greeting in Hamsterish'
-  hamsterHuey.takeDamage(); // returns 'Hamster Huey took damage.'
-  hamsterHuey.destroy(); // returns 'Game object was removed from the game.'
-*/
 
 /* eslint-disable no-undef */
 
